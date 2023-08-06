457e4514be1bfcd5a5503e6ea3a893680d80508e
PotatoTheDevil <62916688+PotatoTheDevil@users.noreply.github.com>
2023-08-06 22:29:39 +0100

    Update neofetch.js

--- js/cmds/neofetch.js
+++ js/cmds/neofetch.js
@@ -3,7 +3,7 @@ export default function(output) {
 <span data-color="#a192e8" class="nomobile">88                     88</span>    <span data-color="#a192e8">hel</span><span data-color="white">@</span><span data-color="pink">web</span>
 <span data-color="#a192e8" class="nomobile">88                     88</span>    <span data-color="white">-------</span>
 <span data-color="#a192e8" class="nomobile">88                     88</span>    <span data-color="pink">OS:</span> <span data-color="white">HEL Linux x86_64</span>
-<span data-color="#a192e8" class="nomobile">88,dPPYba,   ,adPPYba, 88</span>    <span data-color="pink">Kernel:</span> <span data-color="white">6.9.4.20-TOMATO</span> 
+<span data-color="#a192e8" class="nomobile">88,dPPYba,   ,adPPYba, 88</span>    <span data-color="pink">Kernel:</span> <span data-color="white">6.9.4.20-HEL</span> 
 <span data-color="#a192e8" class="nomobile">88P'    "8a a8P     88 88</span>    <span data-color="pink">Uptime:</span> <span data-color="white">69 hours.</span>                                                
 <span data-color="#a192e8" class="nomobile">88       88 8PP""""""  88</span>    <span data-color="pink">CPU:</span> <span data-color="white">Intel i7-7700 CPU @ 3.60GHz</span>
 <span data-color="#a192e8" class="nomobile">88       88 "8b,    ,. 88</span>    <span data-color="pink">GPU:</span> <span data-color="white">GTX 1050-TI 4GB</span> 
